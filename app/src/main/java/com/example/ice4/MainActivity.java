d1ea1cac1c6166f9e12ef38b15127de2341e3dad
Dewald-ST10119314 <ST10119314@vcconnect.edu.za>
2022-05-30 12:05:12 +0200

    Start of OnClicks

--- app/src/main/java/com/example/ice4/MainActivity.java
+++ app/src/main/java/com/example/ice4/MainActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtName, edtDesc;
     Button btnAdd;
+    ImageView imgFruit;
+
+    FruitObj fruit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,13 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String name = edtName.getText().toString();
+                String desc = edtDesc.getText().toString();
+                fruit = new FruitObj(name, desc);
                 Intent i = new Intent(getApplicationContext(), View_Fruit.class);
+                i.putExtra("name", name);
+                i.putExtra("desc", desc);
+
             }
         });
     }
@@ -36,5 +46,6 @@ public class MainActivity extends AppCompatActivity {
         edtName = (EditText) findViewById(R.id.edtName);
         edtDesc = (EditText) findViewById(R.id.edtDesc);
         btnAdd = (Button) findViewById(R.id.btnAddNew);
+        imgFruit = (ImageView) findViewById(R.id.imgFruit);
     }
 }
--- app/src/main/java/com/example/ice4/View_Fruit.java
+++ app/src/main/java/com/example/ice4/View_Fruit.java
@@ -2,13 +2,44 @@ package com.example.ice4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class View_Fruit extends AppCompatActivity {
-
+    TextView txtName, txtDesc;
+    ImageView imgFruit;
+    Button btnImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_fruit);
+
+        setUpUI();
+
+        setUpClickListeners();
+    }
+
+    private void setUpClickListeners() {
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void setUpUI() {
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtDesc = (TextView) findViewById(R.id.txtDesc);
+        imgFruit = (ImageView) findViewById(R.id.imgFruit);
+        btnImage = (Button) findViewById(R.id.btnImage);
+
+        Intent i = getIntent();
+        txtName.setText(i.getStringExtra("name"));
+        txtDesc.setText(i.getStringExtra("desc"));
     }
 }
